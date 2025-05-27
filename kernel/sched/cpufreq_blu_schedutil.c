85166d85607ef2d3b2c1aab5c742871da03edc8b
NekoTuru10 <kirmansyah9696@gmail.com>
2025-05-27 10:27:24 +0000

    Revert "cpufreq: blu_schedutil: Expose default rate-limits as config options"
    This reverts commit 99cad5d2b7f0c82ee719df6694204b4a34908d5d.

--- kernel/sched/cpufreq_blu_schedutil.c
+++ kernel/sched/cpufreq_blu_schedutil.c
@@ -14,7 +14,6 @@
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#include <linux/binfmts.h>
 #include <linux/cpufreq.h>
 #include <linux/kthread.h>
 #include <uapi/linux/sched/types.h>
@@ -466,9 +465,6 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
-	if (task_is_booster(current))
-		return count;
-
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
@@ -488,9 +484,6 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
-
-	if (task_is_booster(current))
-		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
@@ -704,9 +697,9 @@ static int sugov_init(struct cpufreq_policy *policy)
 	}
 
 	tunables->up_rate_limit_us =
-                                CONFIG_BLU_SCHEDUTIL_UP_RATE_LIMIT;
+				cpufreq_policy_transition_delay_us(policy);
 	tunables->down_rate_limit_us =
-                                CONFIG_BLU_SCHEDUTIL_DOWN_RATE_LIMIT;
+				cpufreq_policy_transition_delay_us(policy);
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
